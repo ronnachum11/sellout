49f02891df07765d7ae37964f6409310cb1335ee
Vignav Ramesh <rvignav@gmail.com>
2023-10-14 20:05:24 -0700

    Update app.py

--- Landing/app.py
+++ Landing/app.py
@@ -57,39 +57,22 @@ def start_ai():
 
 
     print("\n\n\nCrafting Email Drafts...")
-    # email_gen = EmailGenerator(
-    #     num_iterations=0,
-    #     initial_prompt="""
-    #     You are an email-writing assistant that writes
-    #     first-contact emails to potential clients.
-    #     """,
-    #     user_name=data['user_name'],
-    #     company_name=data['company_name'],
-    #     company_kb_id=company_kb_id,
-    #     customer_company_name=customer_data[0]['company'],
-    #     customer_name=customer_data[0]['name'],
-    #     customer_url=[customer_data[0]['company_website']]
-    # )
+    email_gen = EmailGenerator(
+        num_iterations=0,
+        initial_prompt="""
+        You are an email-writing assistant that writes
+        first-contact emails to potential clients.
+        """,
+        user_name=data['user_name'],
+        company_name=data['company_name'],
+        company_kb_id=company_kb_id,
+        customer_company_name=customer_data[0]['company'],
+        customer_name=customer_data[0]['name'],
+        customer_url=[customer_data[0]['company_website']]
+    )
     time.sleep(40)
 
-    msg = """Dear Ron,
-
-I trust this message finds you well. I'm Vignav from Open A I, an organization that holds trust, privacy, and data security at its core. We ensure that our clients maintain complete ownership and control of their data, and we strictly do not use customer data for training our models. 
-
-We are well aware of Jane Street's sophisticated, research-driven approach to trading. Our aim is not to replace your existing methodologies, but rather to offer supplementary AI solutions.
-
-Our primary offerings include:
-
-1. ChatGPT Enterprise: This solution is designed for businesses like Jane Street, offering controls, deployment tools, and speed to enhance productivity. It ensures data security through encryption both at rest and in transit, aligning with privacy laws like GDPR. 
-
-2. API Platform: This platform provides access to our powerful models like GPT-4 and GPT-3.5 Turbo. It enables fine-tuning of models for specific tasks using your custom data, ensuring the solutions are tailored to your needs. 
-
-Please let us know a convenient time for you, and we can arrange a call or meeting.
-
-Best Regards,
-
-Vignav
-Open A I Team"""
+    msg = email_gen.generate()
     print("Email Drafts Complete")
 
     print("\n\n\nSending Emails")
@@ -117,12 +100,6 @@ Open A I Team"""
     print("Emails Sent")
     
     time.sleep(40)
-    customer_reply = """Hi Vignav, thanks for reaching out! Wanted to ask more about ChatGPT enterprise security - as a quantitative trading firm dealing with a lot of sensitive financial information, we need to make sure that our data is secure. Have you been audited for compliance with data security standards?
-    
-    Would also love to discuss this more by phone - my number is 7043510608. 
-    
-    Look forward to chatting,
-    Ron"""
     print("\n\n\nReplying to Response...")
     print("Reply Sent")
 
@@ -139,18 +116,16 @@ def call_customer(customer, content):
     # Initialize the Twilio Client
     client = Client(account_sid, auth_token)
 
-    # company_kb_id = 'd614a15a-4c62-4d03-861b-2ec7d82bbfe2'
-    # company_name = customer['company']
-    # company_tool = create_query_tool(f"{company_name} website", f"Get info about {company_name} by asking ACTUAL QUESTIONS, e.g. 'What is ChatGPT for Enterprise' instead of 'ChatGPT for enterprise'", company_kb_id)
-    # company_llm = initialize_agent(
-    #     [company_tool], ChatOpenAI(model_name=GPT_VERSION, temperature=0.9, max_tokens=1000), agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = False
-    # )
-    # text_to_read = company_llm.run(f"Write a phone call introductory message that responds to the following message: {content}.")
-    text_to_read = "Good evening! My name is Vignav, I'm a Sales Team Lead at Open A I. I'm calling to respond to the question you mentioned in your recent email and chat a bit more about Open A I enterprise with you - is now a good time?"
+    company_kb_id = 'd614a15a-4c62-4d03-861b-2ec7d82bbfe2'
+    company_name = customer['company']
+    company_tool = create_query_tool(f"{company_name} website", f"Get info about {company_name} by asking ACTUAL QUESTIONS, e.g. 'What is ChatGPT for Enterprise' instead of 'ChatGPT for enterprise'", company_kb_id)
+    company_llm = initialize_agent(
+        [company_tool], ChatOpenAI(model_name=GPT_VERSION, temperature=0.9, max_tokens=1000), agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose = False
+    )
+    text_to_read = company_llm.run(f"Write a phone call introductory message that responds to the following message: {content}.")
     print('START TEXT')
     print(text_to_read)
     print('END TEXT')
-    # text_to_read = f"Hi {customer_name}, thanks for agreeing to talk to me about {company_name}'s offerings!"
     twiml = str('''<?xml version="1.0" encoding="UTF-8"?>
                 <Response>
                 <Say voice="en-US-Standard-A">''' + text_to_read + '''</Say>
